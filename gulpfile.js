5d5b8e935a0f76e6dc820953d8edeac57c83da2f
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2019-05-14 12:58:55 +0500

    Gulp css compress with gulp-cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-// var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
 // var autoprefixer = require('gulp-autoprefixer');
 // var sourcemaps = require('gulp-sourcemaps');
 
 function cssCopy(done) {
   gulp.src('./css/**/*.css')
     .on('error', console.error.bind(console))
+    .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./www/css/'));
   done();
