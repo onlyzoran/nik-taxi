153494038f6d5633dfb72ec53e3e623b1cc677c7
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2019-05-14 13:04:37 +0500

    Gulp css autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
-// var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer');
 // var sourcemaps = require('gulp-sourcemaps');
 
 function cssCopy(done) {
   gulp.src('./css/**/*.css')
     .on('error', console.error.bind(console))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./www/css/'));
