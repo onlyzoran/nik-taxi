b221648aa16be90baf08414c877934eaa0c8cb42
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2019-05-14 12:42:23 +0500

    Gulp copy all css files to www/css

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+// var rename = require('gulp-rename');
+// var sass = require('gulp-sass');
+// var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps = require('gulp-sourcemaps');
+
+function cssCopy(done) {
+  gulp.src('./css/**/*.css')
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('./www/css/'));
+  done();
+}
+
+function watchCssCopy() {
+  gulp.watch("./css/**/*", cssCopy);
+}
+
+gulp.task('default', watchCssCopy);
